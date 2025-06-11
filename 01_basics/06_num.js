cb2aa59320ead6933e80a30029741ba38c824422
Samrudhi Patil <157829127+Samrudhip84@users.noreply.github.com>
2025-06-11 09:00:18 +0000

    Math and Num

---
+++ 01_basics/06_num.js
@@ -0,0 +1,40 @@
+const score=500
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+const otherNumber=28.34
+// console.log(otherNumber.toPrecision(8));
+
+
+const otherNumber2=20000000
+// console.log(otherNumber2.toLocaleString('en-IN'));
+
+
+///---------MATHS--------//
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(895.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(8.2));
+// console.log(Math.min(8,6,9,2,1))
+// console.log(Math.max(8,6,9,7));
+
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1) + min))
+
+
+
+
+

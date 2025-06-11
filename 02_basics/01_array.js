8d9e667f03e064e647cf3f87a21ba5139cd4c562
Samrudhi Patil <157829127+Samrudhip84@users.noreply.github.com>
2025-06-11 10:19:43 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+//arrays
+
+const myArr=[0,1,3,5,8]
+const myArr2= new Array(9,10)
+// console.log(myArr2);
+
+
+//array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(7)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+// const newArr=myArr.join()
+
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice and splice
+
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
+
+
